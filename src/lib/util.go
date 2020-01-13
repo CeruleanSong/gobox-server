944767baabac4f8c022be99e4a60526f2ce054db
Elias Mawa <elias@emawa.io>
2020-01-12 21:37:44 -0700

    add util func

---
+++ src/lib/util.go
@@ -0,0 +1,15 @@
+package util
+
+import "crypto/rand"
+
+// GenerateRandomBytes generates an specified number of random bytes
+func GenerateRandomBytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	// Note that err == nil only if we read len(b) bytes.
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
